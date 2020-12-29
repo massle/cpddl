8468b8963d3c843c21603b59e78ea7ac879f61c8
Daniel Fiser <danfis@danfis.cz>
2020-12-29 13:45:02 +0100

    Fixed lifted-mgroups test

--- src/lifted_mgroup.c
+++ src/lifted_mgroup.c
@@ -277,21 +277,22 @@ void pddlLiftedMGroupRemoveFixedAtoms(pddl_lifted_mgroup_t *mg)
 }
 
 
+#define MAX_LINE_SIZE 1024
 void printMGroup(const pddl_t *pddl,
                  const pddl_lifted_mgroup_t *mgroup,
                  FILE *fout,
                  bor_err_t *err)
 {
-    char line[256];
+    char line[MAX_LINE_SIZE];
     int used = 0;
-    used += snprintf(line, 256 - used, "{");
+    used += snprintf(line, MAX_LINE_SIZE - used, "{");
 
     for (int i = 0; i < mgroup->cond.size; ++i){
         if (i > 0)
-            used += snprintf(line + used, 256 - used, ", ");
+            used += snprintf(line + used, MAX_LINE_SIZE - used, ", ");
 
         pddl_cond_atom_t *atom = PDDL_COND_CAST(mgroup->cond.cond[i], atom);
-        used += snprintf(line + used, 256 - used,
+        used += snprintf(line + used, MAX_LINE_SIZE - used,
                          "%s", pddl->pred.pred[atom->pred].name);
         for (int j = 0; j < atom->arg_size; ++j){
             if (atom->arg[j].param >= 0){
@@ -300,24 +301,26 @@ void printMGroup(const pddl_t *pddl,
                 ASSERT(!pddlTypesAreDisjunct(&pddl->type, p->type,
                             pddl->pred.pred[atom->pred].param[j]));
                 if (p->is_counted_var){
-                    used += snprintf(line + used, 256 - used, " C%d:%s",
+                    used += snprintf(line + used, MAX_LINE_SIZE - used,
+                                     " C%d:%s",
                                      param_id, pddl->type.type[p->type].name);
                 }else{
-                    used += snprintf(line + used, 256 - used, " V%d:%s",
+                    used += snprintf(line + used, MAX_LINE_SIZE - used,
+                                     " V%d:%s",
                                      param_id, pddl->type.type[p->type].name);
                 }
             }else{
-                used += snprintf(line + used, 256 - used, " %s",
+                used += snprintf(line + used, MAX_LINE_SIZE - used, " %s",
                                  pddl->obj.obj[atom->arg[j].obj].name);
             }
         }
     }
 
-    used += snprintf(line + used, 256 - used, "}");
+    used += snprintf(line + used, MAX_LINE_SIZE - used, "}");
     if (mgroup->is_exactly_one)
-        used += snprintf(line + used, 256 - used, ":=1");
+        used += snprintf(line + used, MAX_LINE_SIZE - used, ":=1");
     if (mgroup->is_static)
-        used += snprintf(line + used, 256 - used, ":S");
+        used += snprintf(line + used, MAX_LINE_SIZE - used, ":S");
 
     if (fout != NULL)
         fprintf(fout, "%s\n", line);
