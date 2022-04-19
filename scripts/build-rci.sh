e779e728c9eca5e4fa42f7fbc9f57e77dd6e1283
Daniel Fiser <danfis@danfis.cz>
2022-04-19 11:57:14 +0200

    Updated build-rci script

--- scripts/build-rci.sh
+++ scripts/build-rci.sh
@@ -14,24 +14,29 @@
 
 set -x
 
-module load GCCcore/9.3.0
-module load Autotools/20180311-GCCcore-9.3.0
+module load Autotools/20200321-GCCcore-10.2.0
+module load Clang/11.0.1-GCCcore-10.2.0
 
 NCPUS=8
 
-CPLEX_ROOT=/mnt/appl/software/CPLEX/12.9-foss-2018b
+#CPLEX_ROOT=/mnt/appl/software/CPLEX/12.9-foss-2018b
+CPLEX_ROOT=/home/fiserdan/cplex/v22.1.0
+#CPLEX_ROOT=/home/fiserdan/cplex/v12.10
 
 cat >Makefile.local <<EOF
+CC = clang
+CXX = clang++
 CFLAGS = -march=native
-CPLEX_CFLAGS = -I$CPLEX_ROOT/cplex/include
-CPLEX_LDFLAGS = -L$CPLEX_ROOT/cplex/bin/x86-64_linux -Wl,-rpath=$CPLEX_ROOT/cplex/bin/x86-64_linux -lcplex1290
-CPOPTIMIZER_CPPFLAGS = -I$CPLEX_ROOT/cpoptimizer/include -I$CPLEX_ROOT/concert/include/ -I$CPLEX_ROOT/cplex/include/
-CPOPTIMIZER_LDFLAGS = -L$CPLEX_ROOT/cpoptimizer/lib/x86-64_linux/static_pic/ -lcp -L$CPLEX_ROOT/concert/lib/x86-64_linux/static_pic/ -lconcert -lstdc++
+IBM_CPLEX_ROOT=$CPLEX_ROOT
+#CPLEX_CFLAGS = -I$CPLEX_ROOT/cplex/include
+#CPLEX_LDFLAGS = -L$CPLEX_ROOT/cplex/bin/x86-64_linux -Wl,-rpath=$CPLEX_ROOT/cplex/bin/x86-64_linux -lcplex1290
+#CPOPTIMIZER_CPPFLAGS = -I$CPLEX_ROOT/cpoptimizer/include -I$CPLEX_ROOT/concert/include/ -I$CPLEX_ROOT/cplex/include/
+#CPOPTIMIZER_LDFLAGS = -L$CPLEX_ROOT/cpoptimizer/lib/x86-64_linux/static_pic/ -lcp -L$CPLEX_ROOT/concert/lib/x86-64_linux/static_pic/ -lconcert -lstdc++
 EOF
 
 
 make mrproper
-make -j$NCPUS opts
+#make -j$NCPUS opts
 make -j$NCPUS bliss
 make -j$NCPUS cudd
 make -j$NCPUS sqlite
