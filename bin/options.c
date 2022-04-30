85a9e22f895fe0db7647b6ceb8d0a90df452c1c3
Daniel Fiser <danfis@danfis.cz>
2022-04-30 14:06:06 +0200

    Small fix in bin/options

--- bin/options.c
+++ bin/options.c
@@ -441,9 +441,9 @@ static void setGroundOptions(void)
     optsStartGroup("Grounding:");
     optsAddIntSwitch("ground", 'G', &opt.ground.method,
                      "Grounding method, one of:\n"
-                     "  dl - datalog-based grounding method (default)\n",
+                     "  dl - datalog-based grounding method (default)\n"
                      "  sql - sqlite-based grounding method\n"
-                     "  trie - default grounding method"
+                     "  trie - default grounding method",
                      4,
                      "trie", GROUND_TRIE,
                      "sql", GROUND_SQL,
