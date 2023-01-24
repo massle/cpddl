e7b3bc3f356727dda6248635e859f9096a3f8c3e
Daniel Fiser <danfis@danfis.cz>
2023-01-24 20:27:34 +0100

    subprocess: execvp with optional time and memory limits

--- pddl/subprocess.h
+++ pddl/subprocess.h
@@ -22,16 +22,26 @@ extern "C" {
 #endif /* __cplusplus */
 
 struct pddl_exec_status {
+    /** True if the sub-processes terminated by exit() */
     int exited;
+    /** If .exited is true, .exit_status is set to the exit status of the
+     *  sub-process */
     int exit_status;
+    /** True if the sub-process was terminated by a signal */
     int signaled;
+    /** If .signaled is true, .signum is set to the signal number that
+     *  terminated the sub-process */
     int signum;
 };
 typedef struct pddl_exec_status pddl_exec_status_t;
 
 
 /**
- * TODO
+ * Runs the given command as a sub-process.
+ * The status struct is filled with the exit status of the sub-process.
+ * The buffer write_stdin is written to the standard input of the
+ * sub-process, and buffers read_stdout and read_stderr are allocated and
+ * filled with stdout and stderr outputs of the subprocess.
  */
 int pddlExecvp(char *const argv[],
                pddl_exec_status_t *status,
@@ -44,7 +54,30 @@ int pddlExecvp(char *const argv[],
                pddl_err_t *err);
 
 /**
- * TODO
+ * Same as pddlExecvp() but allows to set time and memory limit -- negative
+ * numbers mean "unlimited".
+ */
+int pddlExecvpLimits(char *const argv[],
+                     pddl_exec_status_t *status,
+                     const char *write_stdin,
+                     int write_stdin_size,
+                     char **read_stdout,
+                     int *read_stdout_size,
+                     char **read_stderr,
+                     int *read_stderr_size,
+                     float time_limit_in_s,
+                     int mem_limit_in_mb,
+                     pddl_err_t *err);
+
+/**
+ * Run the function fn() in a subprocess and communicate input and output
+ * via a shared memory.
+ * Before fork(), data_size bytes of shared memory is allocated and
+ * in_out_data is copied there. Then fn() is called with the pointer to
+ * that memory, and after return the shared memory is copied back to
+ * in_out_data.
+ * userdata is passed to fn() as its second argument as it is.
+ * status is filled with the exit status of the sub-process.
  */
 int pddlForkSharedMem(int (*fn)(void *sharedmem, void *userdata),
                       void *in_out_data,
@@ -54,7 +87,11 @@ int pddlForkSharedMem(int (*fn)(void *sharedmem, void *userdata),
                       pddl_err_t *err);
 
 /**
- * TODO
+ * Run fn() in a subprocess and collect output written by the function to
+ * the file descriptor fdout, i.e., fn() can write to fdout and the parent
+ * process collects the output into out buffer.
+ * userdata is passed to fn() as its second argument as it is.
+ * status is filled with the exit status of the sub-process.
  */
 int pddlForkPipe(int (*fn)(int fdout, void *userdata),
                  void *userdata,
