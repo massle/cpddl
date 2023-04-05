5586fc92c5560faf9f1daffd3fccb7ff2a80181d
Daniel Fiser <danfis@danfis.cz>
2023-04-05 10:01:33 +0200

    mergesort: Pragma removing warning/error about zero-pointer arithmetics

--- src/mergesort.c
+++ src/mergesort.c
@@ -81,6 +81,7 @@ insertionsort(u_char *a, size_t n, size_t size,
  * boundaries.
  */
 /* Assumption: PSIZE is a power of 2. */
+#pragma clang diagnostic ignored "-Wgnu-null-pointer-arithmetic"
 #define EVAL(p) (u_char **)                        \
     ((u_char *)0 +                            \
         (((u_char *)p + PSIZE - 1 - (u_char *) 0) & ~(PSIZE - 1)))
