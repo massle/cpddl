c9993abd325ce741f55fedb3e0f5a40460755a0f
Daniel Fiser <danfis@danfis.cz>
2023-04-25 18:37:49 +0200

    Improved gitlab pipeline

--- scripts/gitlab-ci-test.sh
+++ scripts/gitlab-ci-test.sh
@@ -7,6 +7,7 @@
         --symba-out - | grep -q "Cost: 7$"
 
 ./bin/pddl pddl-data/ipc-2011/seq-opt/barman/pfile01-001 \
+        --pddl-compile-in-lmg \
         --h2 \
         --P-opm op-fact=2,prune-method=max \
         --gplan astar \
